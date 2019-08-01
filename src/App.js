42133abbebf24fe6b4d0ad0b396ae10e4863740f
sklinov <sergeyklinov@gmail.com>
2019-08-01 16:53:41 +0500

    logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Logo from './components/Logo'
 
 function App() {
   return (
     <div className="App">
-      SEND SAY app
+      <Logo />
     </div>
   );
 }
